44c1ee0907b105bfe8f57484d725f98a0ac1e04a
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2023-01-18 19:48:01 -0500

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name?");
+            var username = Console.ReadLine();
+            Console.WriteLine($"\nHello {username}!");
+            DateTime now = DateTime.Now;
+            int dayOfYear = now.DayOfYear;
+
+            Console.Write("Day of year: ");
+            Console.WriteLine(dayOfYear);
+        }
+    }
+}
